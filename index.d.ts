7f0fbfe108f9ad146d466742d56a70208d70886f
Andrew Luca <thendrluca@gmail.com>
2019-10-09 17:26:33 +0300

    fix: correct flow and typescript

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,2 @@
-export function loader(filePath: string): [];
+type StringValuesObject = { [key: string]: string }
+export function loader<T extends StringValuesObject = any>(filePath: string): T[];
--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ export { default } from './macro';
  * Note: This is a workaround for flow type to work.
  * The exposed functions are not been used in macro library.
  */
-export const loader = (filePath: string): [] => [];
+export function loader(filePath: string): any[] {
+  return [];
+}
